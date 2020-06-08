b3967949ba9bea6f891a393bcec71becfd906ee1
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-06-08 11:03:44 -0300

    Imports optimization

--- src/main/java/br/com/codenation/central_de_erros/CentralDeErrosApplication.java
+++ src/main/java/br/com/codenation/central_de_erros/CentralDeErrosApplication.java
@@ -1,10 +1,8 @@
 package br.com.codenation.central_de_erros;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
