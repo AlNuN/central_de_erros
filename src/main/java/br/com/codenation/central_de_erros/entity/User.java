842c2401dfb73d3f7fc212c7dce07aaae9539870
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-06-15 11:46:46 -0300

    Adding user for authentication purposes

---
+++ src/main/java/br/com/codenation/central_de_erros/entity/User.java
@@ -0,0 +1,74 @@
+package br.com.codenation.central_de_erros.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collection;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Email
+    @NotNull
+    private String email;
+
+    @Column
+    @NotNull
+    private String password;
+
+    @Column
+    @JsonIgnore
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
