fad5d85cbdc47b6ad07723d8156e83c3f8195b52
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-06-15 11:50:15 -0300

    removed security constraints

--- src/main/java/br/com/codenation/central_de_erros/CentralDeErrosApplication.java
+++ src/main/java/br/com/codenation/central_de_erros/CentralDeErrosApplication.java
@@ -2,12 +2,11 @@ package br.com.codenation.central_de_erros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableJpaAuditing
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableSpringDataWebSupport
 public class CentralDeErrosApplication {
 
