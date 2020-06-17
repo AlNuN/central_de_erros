5bf7971c56da5a66691f3ed7b48f61d720b5e578
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-06-17 13:52:43 -0300

    Added handlers for: DataIntegrityViolation, ConstraintViolation and HttpMessageNot readable exceptions.

--- src/main/java/br/com/codenation/central_de_erros/advice/ApplicationAdvices.java
+++ src/main/java/br/com/codenation/central_de_erros/advice/ApplicationAdvices.java
@@ -3,14 +3,17 @@ package br.com.codenation.central_de_erros.advice;
 import br.com.codenation.central_de_erros.controller.EventController;
 import br.com.codenation.central_de_erros.exception.EventNotFoundException;
 import br.com.codenation.central_de_erros.exception.WrongUserInputException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.ConstraintViolationException;
 import java.time.format.DateTimeParseException;
 
 @ControllerAdvice
@@ -47,6 +50,27 @@ public class ApplicationAdvices  {
     @ResponseBody
     @ExceptionHandler(IllegalStateException.class)
     ResponseEntity<Resource<ApiError>> IllegalStateExceptionHandler(IllegalStateException ex) {
+        ApiError error = new ApiError(HttpStatus.BAD_REQUEST, getCause(ex).getMessage());
+        return buildResponseEntity(error);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    ResponseEntity<Resource<ApiError>> DataIntegrityExceptionHandler(DataIntegrityViolationException ex) {
+        ApiError error = new ApiError(HttpStatus.BAD_REQUEST, getCause(ex).getMessage());
+        return buildResponseEntity(error);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    ResponseEntity<Resource<ApiError>> ConstraintViolationHandler(ConstraintViolationException ex) {
+        ApiError error = new ApiError(HttpStatus.BAD_REQUEST, getCause(ex).getMessage());
+        return buildResponseEntity(error);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    ResponseEntity<Resource<ApiError>> HttpMessageNotReadableHandler(HttpMessageNotReadableException ex) {
         ApiError error = new ApiError(HttpStatus.BAD_REQUEST, getCause(ex).getMessage());
         return buildResponseEntity(error);
     }
